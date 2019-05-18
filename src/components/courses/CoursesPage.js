d10f9f00c699b480a6302624e0bfa5c0b32b58e2
Sophia Kiryakova <sophia.kiryakova@gmail.com>
2019-05-18 15:28:54 +0300

    Added redux. Added action creators for creating a course.

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,9 +1,72 @@
 import React from "react";
+import { connect } from "react-redux";
+import * as courseActions from "../../redux/actions/courseActions";
+import Proptypes from "prop-types";
+import { bindActionCreators } from "redux";
 
 class CoursesPage extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      course: {
+        title: ""
+      }
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    const course = { ...this.state.course, title: event.target.value };
+    this.setState({
+      course
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.props.courseActions.createCourse(this.state.course);
+  }
+
   render() {
-    return <h1>Courses</h1>;
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <h2>Courses</h2>
+        <h3>Add course</h3>
+        <input
+          type="text"
+          onChange={this.handleChange}
+          value={this.state.course.title}
+        />
+        <input type="submit" value="Save" />
+        {this.props.courses.map(course => (
+          <div key={course.title}>{course.title}</div>
+        ))}
+      </form>
+    );
   }
 }
 
-export default CoursesPage;
+CoursesPage.propTypes = {
+  courses: Proptypes.array.isRequired,
+  courseActions: Proptypes.object.isRequired
+};
+
+function mapStateToProps(state, props) {
+  return {
+    courses: state.courses
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    courseActions: bindActionCreators(courseActions, dispatch)
+  };
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(CoursesPage);
